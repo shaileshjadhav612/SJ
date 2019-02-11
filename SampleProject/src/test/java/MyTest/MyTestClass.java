e460cb72841687c71d2f10f7d5c93cb4175d90d1
shailesh.jadhav <shailesh.jadhav@RPSPLDP1-60.realitypremedia.in>
2019-02-11 15:10:34 +0530

    Sample

---
+++ SampleProject/src/test/java/MyTest/MyTestClass.java
@@ -0,0 +1,15 @@
+package MyTest;
+
+import org.testng.annotations.Test;
+
+public class MyTestClass {
+	
+	@Test
+	public void  SampleMethods()
+	{
+		
+		
+		System.out.println("******************Successful*****************************");
+	}
+
+}
